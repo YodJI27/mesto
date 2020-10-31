9ba8149fa3e052fc9f68394e70598b236ad6ebe1
YodJI <yodji62@yandex.ru>
2020-10-31 22:04:28 +0300

    На проверку

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, 'dist'),
-        publicPath: "/"
+        publicPath: ""
     },
     // devServer: {
     //     contentBase: path.join(__dirname, 'dist')
